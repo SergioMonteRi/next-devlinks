fe74d19523ee536512f0d3ba89b8cf7977a93978
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-12-03 23:54:44 -0300

    feat: integrate Radix UI switch component for theme toggling and add new icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,57 @@
 'use client'
 
+import { MoonStarsIcon, SunIcon } from '@phosphor-icons/react'
+import * as Switch from '@radix-ui/react-switch'
 import Link from 'next/link'
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 import { Button } from '@/components'
 
 export default function Home() {
-  const { setTheme } = useTheme()
+  const { setTheme, resolvedTheme } = useTheme()
+
+  const checked = resolvedTheme === 'dark' ? false : true
+
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setMounted(true)
+    }, 100)
+  }, [])
+
+  if (!mounted) {
+    return (
+      <div className="h-6 w-16 rounded-full border backdrop-blur transition-all outline-none" />
+    )
+  }
 
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <button onClick={() => setTheme('dark')}>Dark</button>
-      <button onClick={() => setTheme('light')}>Light</button>
-      <button onClick={() => setTheme('system')}>System</button>
+      <Switch.Root
+        data-slot="switch"
+        className={
+          'relative h-6 w-16 rounded-full border backdrop-blur transition-all outline-none'
+        }
+        checked={checked}
+        onCheckedChange={(newChecked) =>
+          setTheme(newChecked ? 'light' : 'dark')
+        }
+      >
+        <Switch.Thumb
+          data-slot="switch-thumb"
+          className="absolute top-1/2 -left-1 flex size-8 -translate-y-1/2 items-center justify-center rounded-full bg-amber-700 transition-transform data-[state=checked]:translate-x-[36px]"
+        >
+          {resolvedTheme === 'dark' ? (
+            <MoonStarsIcon className="text-white" size={16} />
+          ) : (
+            <SunIcon className="text-black" size={16} />
+          )}
+        </Switch.Thumb>
+      </Switch.Root>
 
-      <div className="w-full max-w-xl">
+      <div className="mt-20 w-full max-w-xl">
         <div className="flex justify-center">
           <Button variant="primary" asChild>
             <Link href="/portfolio">Link</Link>
