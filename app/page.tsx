bbe91ea9fc824f5da26a06f1d58d6d4302ae62bf
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-12-05 19:41:20 -0300

    feat: add generateMetadata function to HomePage for dynamic SEO metadata retrieval from Prismic

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import { asImageSrc } from '@prismicio/client'
 import { SliceZone } from '@prismicio/react'
+import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
 import { Avatar, ThemedBackground, ThemeSwitcher } from '@/components'
@@ -32,4 +34,24 @@ export default async function HomePage() {
       </div>
     </div>
   )
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient()
+  const page = await client.getSingle('home').catch(() => notFound())
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+    openGraph: {
+      images: [
+        {
+          url: asImageSrc(page.data.meta_image) ?? '',
+          width: 800,
+          height: 600,
+          alt: 'DevLinks',
+        },
+      ],
+    },
+  }
 }
